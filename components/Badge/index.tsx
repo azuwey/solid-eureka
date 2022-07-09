828812d40c3c610dbc7b78634333b343a26469c7
David Zarandi <david.zarandi@pm.me>
2022-07-09 19:33:23 +0200

    Updated ui skeleton to fit better on the screen Badge interaction fix on mobile

--- components/Badge/index.tsx
+++ components/Badge/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Badge({ label }: Props) {
   return (
     <button
-      className="w-full py-3 px-3 leading-none text-center whitespace-nowrap align-baseline font-semibold rounded-lg hover:bg-amber-200 active:opacity-75 lg:w-32"
+      className="w-full py-3 px-3 leading-none text-center whitespace-nowrap align-baseline font-semibold rounded-lg bg-white hover:bg-amber-200 active:bg-amber-300 active:opacity-75 lg:w-32"
     >
       {label}
     </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
           <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
             Nationalities
           </h2>
